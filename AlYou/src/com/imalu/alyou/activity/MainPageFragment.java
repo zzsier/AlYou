6674add2d1d9f3ca5a73e835a8bd7ece045bc39c
sazhao <zzsier@zzsier-PC>
2014-11-30 13:33:32 +0800

    First version

---
+++ AlYou/src/com/imalu/alyou/activity/MainPageFragment.java
@@ -0,0 +1,146 @@
+/**
+ * Copyright (C) 2013-2014 EaseMob Technologies. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.imalu.alyou.activity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.easemob.EMCallBack;
+import com.easemob.applib.controller.HXSDKHelper;
+import com.easemob.chat.EMChatManager;
+import com.easemob.chat.EMChatOptions;
+import com.imalu.alyou.R;
+import com.imalu.alyou.AlUApplication;
+
+/**
+ * 设置界面
+ * 
+ * @author Administrator
+ * 
+ */
+public class MainPageFragment extends Fragment implements OnClickListener {
+
+	/**
+	 * 设置新消息通知布局
+	 */
+	private RelativeLayout rl_switch_notification;
+	/**
+	 * 设置声音布局
+	 */
+	private RelativeLayout rl_switch_sound;
+	/**
+	 * 设置震动布局
+	 */
+	private RelativeLayout rl_switch_vibrate;
+	/**
+	 * 设置扬声器布局
+	 */
+	private RelativeLayout rl_switch_speaker;
+
+	/**
+	 * 打开新消息通知imageView
+	 */
+	private ImageView iv_switch_open_notification;
+	/**
+	 * 关闭新消息通知imageview
+	 */
+	private ImageView iv_switch_close_notification;
+	/**
+	 * 打开声音提示imageview
+	 */
+	private ImageView iv_switch_open_sound;
+	/**
+	 * 关闭声音提示imageview
+	 */
+	private ImageView iv_switch_close_sound;
+	/**
+	 * 打开消息震动提示
+	 */
+	private ImageView iv_switch_open_vibrate;
+	/**
+	 * 关闭消息震动提示
+	 */
+	private ImageView iv_switch_close_vibrate;
+	/**
+	 * 打开扬声器播放语音
+	 */
+	private ImageView iv_switch_open_speaker;
+	/**
+	 * 关闭扬声器播放语音
+	 */
+	private ImageView iv_switch_close_speaker;
+
+	/**
+	 * 声音和震动中间的那条线
+	 */
+	private TextView textview1, textview2;
+
+	private LinearLayout blacklistContainer;
+	
+	/**
+	 * 退出按钮
+	 */
+	private Button logoutBtn;
+
+	private EMChatOptions chatOptions;
+ 
+	/**
+	 * 诊断
+	 */
+	private LinearLayout llDiagnose;
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_mainpage, container, false);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		if(savedInstanceState != null && savedInstanceState.getBoolean("isConflict", false))
+            return;
+		
+	}
+
+	
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		}
+	}
+	
+	public void showdetail(){
+		
+	}
+
+	
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if(((MainActivity)getActivity()).isConflict)
+            outState.putBoolean("isConflict", true);
+        super.onSaveInstanceState(outState);
+        
+    }
+}
