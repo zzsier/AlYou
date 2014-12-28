69d38f78195cd47b150e52c0f30b28050835a02f
sazhao <zzsier@zzsier-PC>
2014-12-28 21:17:48 +0800

    Add files for Huanxin conversation

---
+++ AlYou/src/com/imalu/alyou/domain/Friend.java
@@ -0,0 +1,35 @@
+package com.imalu.alyou.domain;
+
+public class Friend{
+	
+	private int id;
+	private String username;
+	private String hxname;
+	private String headpicUrl;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getHxname() {
+		return hxname;
+	}
+	public void setHxname(String hxname) {
+		this.hxname = hxname;
+	}
+	public String getHeadpicUrl() {
+		return headpicUrl;
+	}
+	public void setHeadpicUrl(String headpicUrl) {
+		this.headpicUrl = headpicUrl;
+	}
+	
+}
