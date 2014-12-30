4f4d4cd4a7d5f94be5f725c40741d5efff859c47
Administrator <Administrator@XT1-20141107UYY>
2014-12-30 09:39:36 +0800

    1.粉丝列表中的两个item :contact_item_1   contact_item_2  修改为 fans_item_1 fans_item_2 2.创建粉丝列表的实体类：UserLM.java 3.在NetManager.java里添加接口GuanzhuwodeYonghuLiebiao 4.在request包里创建FansRequest.java写入请求参数。 5.在response包里创建FansResponse.java解析Json对象。 6.修改FanslistFragment.java 8.创建adapter适配器  FansAdatter.java
    7.修改LoginActivity.java,添加获得key的方法。

--- AlYou/src/com/imalu/alyou/activity/FanslistFragment.java
+++ AlYou/src/com/imalu/alyou/activity/FanslistFragment.java
@@ -13,14 +13,30 @@
  */
 package com.imalu.alyou.activity;
 
+import java.util.ArrayList;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.imalu.alyou.AlUApplication;
 import com.imalu.alyou.R;
+import com.imalu.alyou.adapter.FansAdapter;
+import com.imalu.alyou.domain.UserLM;
+import com.imalu.alyou.net.JsonHttpResponseHandler;
+import com.imalu.alyou.net.NetManager;
+import com.imalu.alyou.net.request.FansRequest;
+import com.imalu.alyou.net.response.FansResponse;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 
 
@@ -29,6 +45,8 @@ import android.view.ViewGroup;
  * 
  */
 public class FanslistFragment extends Fragment {
+	private ArrayList<UserLM> fans;
+	private  ListView listView;
 	@Override
 	public View onCreateView(LayoutInflater inflater,
 			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -39,12 +57,50 @@ public class FanslistFragment extends Fragment {
 	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onActivityCreated(savedInstanceState);
+		listView=(ListView)getActivity().findViewById(R.id.lv_fans_list);
+		fansFund();
+		Log.e("FANS",""+fans.toString());
+		FansAdapter adapter=new FansAdapter(getActivity(), fans);
+		listView.setAdapter(adapter);
 	}
-	
-	
-
-
-	
+	//发送请求，查询粉丝数据
+		private void fansFund() {
+			String  key=AlUApplication.getMyInfo().getKey();
+			fans=new ArrayList<UserLM>();
+			FansRequest fansRqst=new FansRequest();
+			fansRqst.setUserkey(key);
+			NetManager.execute(NetManager.FANS_REQUEST_OPERATION, fansRqst, new JsonHttpResponseHandler(){
+				@Override
+				public void onSuccess(int statusCode, Header[] headers,
+						JSONArray response) {
+					// TODO Auto-generated method stub
+					super.onSuccess(statusCode, headers, response);
+					try {
+						getJsonObj(response);
+						Log.e("lory.....", ""+fans.toString());
+						} catch (JSONException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+				}
+			});
+			
+		}
+		//遍历Json数组
+		protected void getJsonObj(JSONArray array) throws JSONException{
+			FansResponse fansRponse=new FansResponse();
+			for(int i=0;i<array.length();i++){
+				JSONObject jsonObject= new JSONObject();
+				jsonObject= array.getJSONObject(i);
+				UserLM fan= new UserLM();
+				fansRponse.setJsonObject(jsonObject);
+				fan.setId(fansRponse.getId());
+				fan.setUserName(fansRponse.getUserName());
+				fan.setGexingQianming(fansRponse.getGexingQianming());
+				fans.add(fan);
+			}
+			
+		}
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
 	    if(((MainActivity)getActivity()).isConflict)
